8ffda056aa1b90e31bbc2426237cd7ee33e4be8f
emildoychinov <bgemo105@gmail.com>
2022-11-13 20:04:41 +0200

    ok...

--- tail.c
+++ tail.c
@@ -2,22 +2,42 @@
 
 int main(int argc, char *argv[]){
 	
-	if(!argv[1]){
+	if(argc < 2){
 		fprintf(stderr, "no arguments provided\n");
-		exit(0);
+		exit(1);
 	}
-	
+
 	int fd = open(argv[1], O_RDONLY);
-	int n = (argc >= 2) ? stoi(argv[2]) : -1; //the number of lines that we want 
+
+	int n = 10; // initial value, that's how our code works
+
+	if(argc > 2){
+		if(m_strcmp(argv[2], "-n")){
+
+			if(argc == 3) {
+				fprintf(stderr, "expected an argument after '-n' flag\n");
+				exit(1);
+			}
+
+			if(argc > 3){
+				n = stoi(argv[3]);
+			}
+		}
+
+		else {
+			fprintf(stderr, "flag '%s' not recognized\n", argv[2]);
+			exit(1);
+		}
+	}
 
 	if(fd == -1){
 		fprintf(stderr, "no such file named %s found\n", argv[1]);
-		exit(0);
+		exit(1);
 	}
 
 	e_lines(fd, n);
 	close(fd);
 
-	exit(1);
+	exit(0);
 
 }
